3eec4edfb1a75c28889a4f85945eba4799938d24
Ahmad <abuallaban.ahmad2002@gmail.com>
2024-07-30 22:02:09 +0500

    tournaments, teams, matches fix

--- src/api/team.js
+++ src/api/team.js
@@ -2,4 +2,5 @@ import api from './api';
 
 export const getTeam = (discipline) => api.get(`/teams/${discipline}/rukh`);
 export const getTeams = (discipline) => api.get(`/teams/${discipline}`);
+export const getAllTeams = () => api.get(`/teams/`);
 export const getPlayer = () => api.get(`/players/`);
